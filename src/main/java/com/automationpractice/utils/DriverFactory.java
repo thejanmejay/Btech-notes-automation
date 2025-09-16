1244cdfdac248ecad3caef4100a8456e83fd6878
thejanmejay <janmejaysingh331@gmail.com>
2025-09-16 14:50:00 +0530

    Redirection

---
+++ src/main/java/com/automationpractice/utils/DriverFactory.java
@@ -0,0 +1,41 @@
+package com.automationpractice.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+	public static WebDriver initDriver(String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver.set(new ChromeDriver());
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver.set(new FirefoxDriver());
+		} else if (browser.equalsIgnoreCase("edge")) {
+			// Use manually downloaded EdgeDriver
+			System.setProperty("webdriver.edge.driver", "C:\\drivers\\msedgedriver.exe");
+			driver.set(new EdgeDriver());
+		} else {
+			throw new IllegalArgumentException("Browser not supported: " + browser);
+		}
+
+		driver.get().manage().window().maximize();
+		return driver.get();
+	}
+
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
+
+	public static void quitDriver() {
+		if (driver.get() != null) {
+			driver.get().quit();
+			driver.remove();
+		}
+	}
+}
--- src/test/java/SampleTest.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.Test;
-
-public class SampleTest {
-    
-    @Test
-    public void testSetup() {
-        System.out.println("TestNG is linked and working!");
-    }
-}
